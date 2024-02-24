f325865b060e600e5fa90b393c47707de338ad77
XxZer0xX <www.eljok87@gmail.com>
2024-02-24 23:23:20 +0100

    update c01

--- c01/ex00/prog/ft_ft.c
+++ c01/ex00/prog/ft_ft.c
@@ -1,27 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.h                                            :+:      :+:    :+:   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdelmeni <eljok87@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/24 19:28:52 by mdelmeni          #+#    #+#             */
-/*   Updated: 2024/02/24 19:28:52 by mdelmeni         ###   ########.fr       */
+/*   Created: 2024/02/24 01:20:22 by mdelmeni          #+#    #+#             */
+/*   Updated: 2024/02/24 01:20:22 by mdelmeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_FT_H
-#define FT_FT_H
 
-void    ft_ft(int *nbr);
-
-#endif
-
-#ifndef FT_FT1_H
-#define FT_FT1_H
-
-void    ft_putchar(char c);
-void	ft_putnbr(int nb);
-
-#endif
-
+void    ft_ft(int *nbr)
+{
+    *nbr =  42;
+}
---
+++ c01/ex00/prog/ft_ft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdelmeni <eljok87@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 19:28:52 by mdelmeni          #+#    #+#             */
+/*   Updated: 2024/02/24 19:28:52 by mdelmeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FT_H
+#define FT_FT_H
+
+void    ft_ft(int *nbr);
+
+#endif
+
+#ifndef FONCTION_H
+#define FONCTION_H
+
+void    ft_putchar(char c);
+void	ft_putnbr(int nb);
+
+#endif
+
