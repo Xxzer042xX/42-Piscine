60b340b7653d76be5d3d35f75f6a31916c9062dd
XxZer0xX <www.eljok87@gmail.com>
2024-02-24 11:13:40 +0100

    update

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -29,6 +29,34 @@ void	ft_rev_int_tab(int *tab, int size)
 		end--;
 	}
 }
+/*
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		if (nb == -2147483648)
+		{
+			ft_putchar('2');
+			nb = -147483648;
+		}
+		nb = -nb;
+	}
+	else if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
 
 int	main(void)
 {
@@ -47,9 +75,10 @@ int	main(void)
 	ft_rev_int_tab(arr, size);
 	while (i < size)
 	{
-		printf("%d ", arr[i]);
+		ft_putnbr(arr[i]);
 		i++;
 	}
-	printf("\n");
+	ft_putchar('\n');
 	return (0);
 }
+*/
