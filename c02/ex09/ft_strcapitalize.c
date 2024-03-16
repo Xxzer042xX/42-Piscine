b3e43da57fb8f28655f2d69603bec4ebff8e22f3
Xxzer042xX <eljok87@gmail.com>
2024-03-16 15:11:11 +0100

    update

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -19,8 +19,8 @@ char	*ft_strcapitalize(char *str)
 	int	i;
 
 	i = 0;
-	//
-    if (str[i] != '\0' && (str[i] >= 'a' && str[i] <= 'z'))
+	// Capitalize the first letter if it's lowercase
+	if (str[i] != '\0' && (str[i] >= 'a' && str[i] <= 'z'))
 		str[i] = str[i] - 32;
 	i += 1;
 	while (str[i] != '\0')
@@ -28,14 +28,18 @@ char	*ft_strcapitalize(char *str)
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			// Convert to uppercase if the previous character is not a letter or a number
-			if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z') || (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || (str[i - 1] >= '0' && str[i - 1] <= '9')))
-				str[i] = str[i] - 32;
+			if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
+				  (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
+				  (str[i - 1] >= '0' && str[i - 1] <= '9')))
+				str[i] -= 32;
 		}
 		else if (str[i] >= 'A' && str[i] <= 'Z')
 		{
 			// Convert to lowercase if the previous character is a letter or a number
-			if ((str[i - 1] >= 'a' && str[i - 1] <= 'z') || (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || (str[i - 1] >= '0' && str[i - 1] <= '9'))
-				str[i] = str[i] + 32;
+			if ((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
+				(str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
+				(str[i - 1] >= '0' && str[i - 1] <= '9'))
+				str[i] += 32;
 		}
 		i++;
 	}
