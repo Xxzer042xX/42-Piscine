6dd82763baaea3a09fb009816e297678ff24d376
Xxzer042xX <eljok87@gmail.com>
2024-03-16 15:07:03 +0100

    update

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -19,7 +19,8 @@ char	*ft_strcapitalize(char *str)
 	int	i;
 
 	i = 0;
-	if (str[i] != '\0' && (str[i] >= 'a' && str[i] <= 'z'))
+	//
+    if (str[i] != '\0' && (str[i] >= 'a' && str[i] <= 'z'))
 		str[i] = str[i] - 32;
 	i += 1;
 	while (str[i] != '\0')
