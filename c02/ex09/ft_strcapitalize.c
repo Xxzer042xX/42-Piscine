579d738b01aabda80be874031f1118a404e0e8c5
Xxzer042xX <eljok87@gmail.com>
2024-03-16 15:55:56 +0100

    update

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -33,12 +33,10 @@ char	*ft_strcapitalize(char *str)
                   (str[i - 1] >= '0' && str[i - 1] <= '9')))
                 str[i] -= 32;
 		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
+        else if (str[i] >= 'A' && str[i] <= 'Z')
 		{
 			// Convert to lowercase if the previous character is a letter or a number
-			if ((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
-            (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
-            (str[i - 1] >= '0' && str[i - 1] <= '9'))
+			if ((str[i - 1] >= 'a' && str[i - 1] <= 'z') || (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || (str[i - 1] >= '0' && str[i - 1] <= '9'))
                 str[i] += 32;
 		}
 		i++;
