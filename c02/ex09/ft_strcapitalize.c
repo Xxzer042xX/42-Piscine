a5fd4a69317bd40026cd0896f02cb68dcde6a091
Xxzer042xX <eljok87@gmail.com>
2024-03-16 15:53:57 +0100

    update

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -37,9 +37,9 @@ char	*ft_strcapitalize(char *str)
 		{
 			// Convert to lowercase if the previous character is a letter or a number
 			if ((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
-                (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
-			    (str[i - 1] >= '0' && str[i - 1] <= '9'))
-				str[i] += 32;
+            (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
+            (str[i - 1] >= '0' && str[i - 1] <= '9'))
+                str[i] += 32;
 		}
 		i++;
 	}
