1bf899cb198436ecedf0b71fc3835b49e70ef323
Xxzer042xX <eljok87@gmail.com>
2024-06-21 11:01:32 +0200

    update c0008

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -10,40 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> // Inclut la bibliothèque unistd.h, nécessaire pour la fonction write
+#include <unistd.h>
 
-// Fonction principale pour générer et afficher toutes les combinaisons de n chiffres
+// Main function to generate and print all combinations of n digits
 void ft_print_combn(int n)
 {
-    int pos = 0;
-    char current = '0';
+    int pos;
+    char current;
     char str[10];
 
-    // Fonction récursive interne pour générer les combinaisons
+    pos = 0;
+    current = '0';
+
+    // Recursive function to generate combinations
     void generate_combinations(int pos, char current)
     {
-        // Si la position actuelle est égale à n, on a généré une combinaison complète
         if (pos == n)
         {
             write(1, str, n);
-            // Si le premier caractère n'est pas le dernier possible pour cette longueur de combinaison, ajoute une virgule et un espace
             if (str[0] != '9' - n + 1)
                 write(1, ", ", 2);
             return;
         }
-
-        str[pos] = current; // Assigne le caractère courant à la position actuelle de la combinaison
-        // Si le caractère courant est inférieur ou égal à '9'
+        str[pos] = current;
         if (current <= '9')
         {
-            // Appelle récursivement la fonction pour la position suivante avec le caractère suivant
             generate_combinations(pos + 1, current + 1);
-            // Si le caractère suivant est encore valide, appelle récursivement la fonction pour la même position avec le caractère suivant
             if (current + 1 <= '9')
                 generate_combinations(pos, current + 1);
         }
     }
-
-    // Début du traitement dans ft_print_combn
-    generate_combinations(pos, current); // Démarre la génération des combinaisons avec les valeurs initialisées
+    generate_combinations(pos, current);
 }
