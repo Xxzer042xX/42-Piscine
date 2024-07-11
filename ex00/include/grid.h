d4ef1d517f6ca9d808efcc3120ee41ce2edfb397
Xxzer042xX <eljok87@gmail.com>
2024-07-12 01:31:43 +0200

    clean repo

--- ex00/include/grid.h
+++
@@ -1,8 +0,0 @@
-#ifndef GRID_H
-#define GRID_H
-
-int ft_checkviews_value(int **grid, int *views_value);
-int ft_isSafe(int **grid, int row, int col, int num);
-int ft_solveGrid(int **grid, int *views_value, int row, int col);
-
-#endif
