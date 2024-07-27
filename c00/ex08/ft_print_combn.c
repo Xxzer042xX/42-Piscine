b5ed5569626f2270f112305507ed593f7e0dff1d
Xxzer042xX <eljok87@gmail.com>
2024-07-27 12:33:42 +0200

    norminette ok

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -29,46 +29,38 @@ int	is_last_comb(char *str, int n)
 
 void	ft_print_combn(int n)
 {
-	char str[n + 1];
-	int stack[n];
-	int pos;
-	int nbr;
+	char	str[10];
+	int		pos;
+	int		nbr;
 
 	str[n] = '\0';
 	pos = 0;
 	nbr = '0';
-	if (n < 1 || n > 9)
-		return ;
 	while (1)
 	{
 		if (pos == n)
 		{
 			write_comb(str, n, is_last_comb(str, n));
-			if (pos == 0)
-				break ;
 			pos--;
-			nbr = stack[pos] + 1;
+			nbr = str[pos] + 1;
 		}
 		else if (nbr <= '9')
-		{
-			str[pos] = nbr;
-			stack[pos] = nbr;
-			pos++;
-			nbr++;
-		}
+			str[pos++] = nbr++;
 		else
 		{
 			if (pos == 0)
 				break ;
 			pos--;
-			nbr = stack[pos] + 1;
+			nbr = str[pos] + 1;
 		}
 	}
 }
-
-int	main(int argc, char **argv)
+/*
+int	main(int ac, char **av)
 {
-	if (argc == 2)
-		ft_print_combn(atoi(argv[1]));
+	if (ac == 2 && av[1][0] > '0' && av[1][0] <= '9')
+		ft_print_combn(atoi(av[1]));
+	write(1, "\n", 1);
 	return (0);
 }
+*/
