faa30c00b0a2cda25eee602f91a919d0dbd4bf8b
Xxzer042xX <eljok87@gmail.com>
2024-08-01 14:04:41 +0200

    norminette ok

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -10,3 +10,83 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int	ft_strlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+void	ft_strcpy(char *dest, const char *src)
+{
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+}
+
+int	calculate_total_length(int size, char **strs, int sep_len)
+{
+	int	total_len;
+	int	i;
+
+	total_len = 0;
+	i = 0;
+	while (i < size)
+	{
+		total_len += ft_strlen(strs[i]);
+		i++;
+	}
+	total_len += sep_len * (size - 1);
+	total_len += 1;
+	return (total_len);
+}
+
+char	*join_strings(int size, char **strs, char *sep, char *join)
+{
+	char	*current_pos;
+	int		i;
+
+	current_pos = join;
+	i = 0;
+	while (i < size)
+	{
+		ft_strcpy(current_pos, strs[i]);
+		current_pos += ft_strlen(strs[i]);
+		if (i < size - 1)
+		{
+			ft_strcpy(current_pos, sep);
+			current_pos += ft_strlen(sep);
+		}
+		i++;
+	}
+	return (join);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*join;
+	int		sep_len;
+	int		total_len;
+
+	if (size == 0)
+	{
+		join = malloc(1);
+		if (!join)
+			return (NULL);
+		*join = '\0';
+		return (join);
+	}
+	sep_len = ft_strlen(sep);
+	total_len = calculate_total_length(size, strs, sep_len);
+	join = malloc(total_len);
+	if (!join)
+		return (NULL);
+	return (join_strings(size, strs, sep, join));
+}
