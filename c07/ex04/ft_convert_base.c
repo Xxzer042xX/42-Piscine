6223cf113c719c24115f1554dacc4461b26a6252
Xxzer042xX <eljok87@gmail.com>
2024-08-02 10:56:27 +0200

    norminette ok

---
+++ c07/ex04/ft_convert_base.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdelmeni <eljok87@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 10:46:23 by mdelmeni          #+#    #+#             */
+/*   Updated: 2024/08/02 10:46:23 by mdelmeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+// Déclaration des fonctions définies dans ft_convert_base2.c
+int		ft_strlen(char *str);
+char	*convert_to_base(int num, char *base);
+
+int	is_valid_char(char c, char *base)
+{
+	if (c == '+' || c == '-' || c <= 32 || c == 127)
+		return (0);
+	while (*base)
+	{
+		if (c == *base++)
+			return (0);
+	}
+	return (1);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (!is_valid_char(base[i], base + i + 1))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	char_to_value(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	result;
+	int	sign;
+	int	base_len;
+	int	value;
+
+	result = 0;
+	sign = 1;
+	base_len = ft_strlen(base);
+	while (*str <= 32)
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str)
+	{
+		value = char_to_value(*str, base);
+		if (value == -1)
+			break ;
+		result = result * base_len + value;
+		str++;
+	}
+	return (result * sign);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int	num;
+
+	if (!is_valid_base(base_from) || !is_valid_base(base_to))
+		return (NULL);
+	num = ft_atoi_base(nbr, base_from);
+	return (convert_to_base(num, base_to));
+}
---
+++ c07/ex04/ft_convert_base2.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdelmeni <eljok87@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 10:46:27 by mdelmeni          #+#    #+#             */
+/*   Updated: 2024/08/02 10:46:27 by mdelmeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	get_num_length(int num, int base_len)
+{
+	int	length;
+
+	length = 0;
+	if (num <= 0)
+		length = 1;
+	while (num != 0)
+	{
+		num /= base_len;
+		length++;
+	}
+	return (length);
+}
+
+void	fill_base_str(char *result, int num, int base_len, char *base)
+{
+	result[--result[0]] = '\0';
+	if (num == 0)
+		result[0] = base[0];
+	else
+	{
+		if (num < 0)
+		{
+			result[0] = '-';
+			num = -num;
+		}
+		while (num)
+		{
+			result[--result[0]] = base[num % base_len];
+			num /= base_len;
+		}
+	}
+}
+
+char	*convert_to_base(int num, char *base)
+{
+	int		base_len;
+	int		length;
+	char	*result;
+
+	base_len = ft_strlen(base);
+	length = get_num_length(num, base_len);
+	result = (char *)malloc(length + 1);
+	if (!result)
+		return (NULL);
+	result[0] = length;
+	fill_base_str(result, num, base_len, base);
+	return (result + 1);
+}
